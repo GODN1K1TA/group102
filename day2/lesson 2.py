4de8202690393861259765e0da5788e06f6455ac
GODN1K1TA <nikaadamashvili7@gmail.com>
2023-06-24 22:02:41 +0400

    day2 commit

--- day2/lesson 2.py
+++ day2/lesson 2.py
@@ -1,1 +1,25 @@
 print ("this is day 2")
+name = "nika"
+print(len(name))
+my_sentence = """გამარჯობა, შეგახსენებთ სწავლას ვიწყებთ სექტემბრიდან,
+ გთხოვთ არ გააცდინოთ"""
+
+print(my_sentence)
+#knows_programming = True
+if 10>5:
+    print("hello")
+
+
+full_name = "nika adamashvili"
+print(len(full_name))
+print(full_name[0])
+print("n" in full_name)
+print(full_name[2:7])
+print(full_name[2:9:3])
+print(full_name[:5])
+print(full_name[5:])
+print(full_name[-4])
+print("ku"+full_name[-1:-4:-1])
+print(full_name.upper())
+name2="  gvantsa  "
+print(name2.replace("a" , "$"))
